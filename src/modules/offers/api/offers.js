6b010b62ba85ee68ce701d417a3873642f34108b
DanDev1004 <dandeveloper1004@gmail.com>
2025-05-30 17:44:24 -0500

    feat: 🎸 offers module - api

---
+++ src/modules/offers/api/offers.js
@@ -0,0 +1,26 @@
+import axios from "../../shared/api/axiosInstance";
+
+export const getAll = async () => {
+    const { data } = await axios.get("/offer");
+    return data;
+};
+
+export const getById = async (id) => {
+    const { data } = await axios.get(`/offer/${id}`);
+    return data;
+};
+
+export const create = async (offer) => {
+    const { data } = await axios.post("/offer", offer);
+    return data;
+};
+
+export const updateById = async (id, offer) => {
+    const { data } = await axios.patch(`/offer/${id}`, offer);
+    return data;
+};
+
+export const deleteById = async (id) => {
+    const { data } = await axios.delete(`/offer/${id}`);
+    return data;
+};
